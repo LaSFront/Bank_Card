a3421276623574427e177d464586468c74243207
“Larisa” <lsergeevna1978@mail.ru>
2023-07-28 15:05:52 +0400

    Fixed

--- src/test/java/ru/netology/web/BankCardTest.java
+++ src/test/java/ru/netology/web/BankCardTest.java
@@ -1,66 +1,69 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.conditions.Value;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
 
 public class BankCardTest {
-    private String dateForMeet = "04.08.2023"; //дата встречи
-    private String monthYear = "август 2023"; //месяц и год (в ".calendar__name")
-    private String dayOfMonth = "4"; //число в дате (в ".calendar__day")
-    private int n = 1; // сколько раз нажать на ">" (для счетчика)
 
     @BeforeEach
     void setUp() {
         open("http://localhost:9999");
     }
 
+    public String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
     @Test
     void shouldSendRequestManualInput() {
+
+        String planningDate = generateDate(4, "dd.MM.yyyy");
 
         $("[data-test-id='city'] input").sendKeys("Ростов-на-Дону");
 
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").sendKeys(dateForMeet);
+        $("[data-test-id='date'] input").sendKeys(planningDate);
 
         $("[data-test-id='name'] input").sendKeys("Иван Иванов-Петров");
         $("[data-test-id='phone'] input").sendKeys("+79000000000");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-
-        $("[data-test-id='notification']").shouldHave(exactText("Успешно! Встреча успешно забронирована на " + dateForMeet), Duration.ofSeconds(15));
+        $$(".notification__title").find(exactText("Успешно!"));
+        $$(".notification__content").find(exactText("Встреча успешно забронирована на " + planningDate));
     }
-
     @Test
     void shouldAutocompleteDateAndCity() {
+        String planningDayOfDate = generateDate(7, "d");
+        String planningDate = generateDate(7, "dd.MM.yyyy");
+
         // город
-        $("[data-test-id='city'] input").sendKeys("ка");
-        $$(".input__menu .menu-item__control").find(exactText("Казань")).click();
-
+        $("[data-test-id='city'] input").sendKeys("ле");
+        $$("div .menu.menu_mode_radio-check.input__menu .menu-item__control").find(exactText("Салехард")).click();
         // календарь
-        $(".icon_name_calendar").click();
-        // счетчик для 'click':
-        for (int i = 0; i < n; i++) {
-            $("[data-step='1'].calendar__arrow").click();
-        }
-        $(".calendar__name").shouldHave(text(monthYear));
-        $$(".calendar__day").find(exactText(dayOfMonth)).click();
-        $("[data-test-id='date'] input").shouldHave(new Value(dateForMeet));
+        $("button .icon_name_calendar").click();
 
+        if(!generateDate(3,"MM").equals(generateDate(7, "MM"))) {
+            $(".calendar__arrow[data-step='1']").click();
+        } else {
+            return;
+        }
+        $$(".calendar__day").find(exactText(planningDayOfDate)).click();
+        // остальные поля
         $("[data-test-id='name'] input").sendKeys("Иван Иванов-Петров");
         $("[data-test-id='phone'] input").sendKeys("+79000000000");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-
-        $("[data-test-id='notification']").shouldHave(exactText("Успешно! Встреча успешно забронирована на " + dateForMeet), Duration.ofSeconds(15));
+        //уведомление
+        $$(".notification__title").find(exactText("Успешно!"));
+        $$(".notification__content").find(exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
